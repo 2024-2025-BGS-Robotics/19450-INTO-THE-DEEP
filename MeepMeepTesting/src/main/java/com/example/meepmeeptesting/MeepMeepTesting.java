8b5ad350e5ea24da0a1ea63fdadb73de22a8fbb4
Punjdoggy <112536823+Punjdoggy@users.noreply.github.com>
2024-09-11 14:07:59 +1000

    Altered Splines to Lineto's and init new Left side entity

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -1,11 +1,9 @@
 package com.example.meepmeeptesting;
 
 import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
-import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedDark;
-import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeRedLight;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueLight;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
@@ -18,45 +16,76 @@ import javax.imageio.ImageIO;
 
 public class MeepMeepTesting {
 
-    private static RoadRunnerBotEntity RedBot1;
+    private static RoadRunnerBotEntity RightSideBlueBot;
+    private static RoadRunnerBotEntity LeftSideBlueBot;
+    private static RoadRunnerBotEntity RightSideRedBot;
+    private static RoadRunnerBotEntity LeftSideRedBot;
 
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(900);
 
-            System.out.println("Start");
+            System.out.println("Init");
 
 
-             RedBot1 = new DefaultBotBuilder(meepMeep)
+             RightSideBlueBot = new DefaultBotBuilder(meepMeep)
                     // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                     // Set bot colour: Blue Dark, Blue Light, Red Dark, Red Light
-                    .setColorScheme(new ColorSchemeRedDark())
+                    .setColorScheme(new ColorSchemeBlueLight())
                     .setConstraints(52, 60, Math.toRadians(180), Math.toRadians(180), 15)
                     .followTrajectorySequence(drive ->
-                            drive.trajectorySequenceBuilder(new Pose2d(35, 63, Math.toRadians(270)))
-                                    .splineToLinearHeading(new Pose2d(0, 33,Math.toRadians(270)), Math.toRadians(0))
+                            drive.trajectorySequenceBuilder(new Pose2d(12, 63, Math.toRadians(270)))
+                                    .lineToLinearHeading(new Pose2d(0, 33,Math.toRadians(270)))
                                     .waitSeconds(2)
                                     .back(4)
-                                    .splineToLinearHeading(new Pose2d(48, 38,Math.toRadians(270)), Math.toRadians(5))
-                                    .splineToLinearHeading(new Pose2d(56, 57,Math.toRadians(-320)), Math.toRadians(-200))
-                                    .splineToLinearHeading(new Pose2d(58, 38,Math.toRadians(270)), Math.toRadians(0))
-                                    .splineToLinearHeading(new Pose2d(56, 57,Math.toRadians(-320)), Math.toRadians(-20))
-                                    .splineToLinearHeading(new Pose2d(58, 27,Math.toRadians(0)), Math.toRadians(-20))
-                                    .splineToLinearHeading(new Pose2d(56, 57,Math.toRadians(-320)), Math.toRadians(0))
+                                    .lineToLinearHeading(new Pose2d(48, 38,Math.toRadians(270)))
+                                    .lineToLinearHeading(new Pose2d(56, 57,Math.toRadians(-320)))
+                                    .lineToLinearHeading(new Pose2d(58, 38,Math.toRadians(270)))
+                                    .lineToLinearHeading(new Pose2d(56, 57,Math.toRadians(-320)))
+                                    .lineToLinearHeading(new Pose2d(58, 27,Math.toRadians(0)))
+                                    .lineToLinearHeading(new Pose2d(56, 57,Math.toRadians(-320)))
                                     .build()
 
                     );
 
-             System.out.println("Finished");
+             LeftSideBlueBot = new DefaultBotBuilder(meepMeep)
+                     .setColorScheme(new ColorSchemeBlueDark())
+                             .setConstraints(52, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                                     .followTrajectorySequence(drive ->
+                                             drive.trajectorySequenceBuilder(new Pose2d(-12,63,Math.toRadians(270)))
+                                                     .lineToLinearHeading(new Pose2d(-48, 38,Math.toRadians(270)))
+                                                     .waitSeconds(1)
+                                                     .lineToLinearHeading(new Pose2d(-48, 58,Math.toRadians(-270)))
+                                                     .waitSeconds(1)
+                                                     .lineToLinearHeading(new Pose2d(-58, 38,Math.toRadians(270)))
+                                                     .waitSeconds(1)
+                                                     .lineToLinearHeading(new Pose2d(-48, 58,Math.toRadians(-270)))
+                                                     .waitSeconds(1)
+                                                     .lineToLinearHeading(new Pose2d(-58,27,Math.toRadians(180)))
+                                                     .waitSeconds(1)
+//                                                     .splineToLinearHeading(new Pose2d(-54, 27,Math.toRadians(180)), Math.toRadians(0))
+//                                                     .waitSeconds(1)
+                                                     .lineToLinearHeading(new Pose2d(-48, 58,Math.toRadians(-270)))
+                                                     .waitSeconds(1)
+                                                     .build()
+                                     );
+
+             System.out.println("Start");
 
         Image img = null;
         try { img = ImageIO.read(new File("MeepMeepTesting/src/resources/Into-The-Deep-Dark.png")); }
-        catch (IOException e) {}
+        catch (IOException ignored) {}
 
         meepMeep.setBackground(img)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(RedBot1)
+                .addEntity(RightSideBlueBot)
+                .addEntity(LeftSideBlueBot)
                 .start();
+
+
     }
+
+
+
 }
 
 
