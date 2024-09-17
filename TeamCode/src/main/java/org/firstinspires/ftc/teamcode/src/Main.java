2526c2030287c657a0850ca66f20d84add9d2f03
Punjdoggy <112536823+Punjdoggy@users.noreply.github.com>
2024-09-17 15:50:20 +1000

    Added Variable Trajectory Init system for Path Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/Main.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/src/Main.java
@@ -7,13 +7,16 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 import com.qualcomm.robotcore.util.ElapsedTime;
 
+import org.firstinspires.ftc.teamcode.R;
 import org.firstinspires.ftc.teamcode.lib.MotorController;
 import org.firstinspires.ftc.teamcode.lib.SliderController;
 
+import java.util.Objects;
 
 @TeleOp (name="LDB Main", group="LinearOpmode")
 public class Main extends LinearOpMode {
     public ElapsedTime runtime = new ElapsedTime();
+    private static String trajectory;
 
     public void runOpMode() {
         telemetry.addData("Status", "Initialized");
@@ -35,7 +38,34 @@ public class Main extends LinearOpMode {
         // run until the end of the match (driver presses STOP)
         while (opModeIsActive()) {
             MotorController.runmotor(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x, gamepad1.right_bumper);
-            roadrunnerhandeler.drive.followTrajectorySequence(RoadRunnerHandler.BlueLeftSideRunner);
+
+            if(gamepad1.dpad_up && gamepad1.a) {
+                trajectory = "BlueLeft";
+            }else if (gamepad1.dpad_up && gamepad1.b){
+                trajectory = "BlueRight";
+            }else if (gamepad1.dpad_up && gamepad1.x) {
+                trajectory = "RedLeft";
+            }else if (gamepad1.dpad_up&& gamepad1.y){
+                trajectory = "RedRight";
+            }
+
+            telemetry.addLine("Chosen Trajectory:" + trajectory);
+            telemetry.addLine("----Press 'x' to run this trajectory----");
+
+            if(gamepad1.x && Objects.equals(trajectory, "BlueLeft")){
+                roadrunnerhandeler.drive.followTrajectorySequence(RoadRunnerHandler.BlueLeftSideRunner);
+                telemetry.addLine("Running Trajectory:"+ trajectory);
+            } else if(gamepad1.x && Objects.equals(trajectory,"BlueRight")){
+                roadrunnerhandeler.drive.followTrajectorySequence(RoadRunnerHandler.BlueRightSideRunner);
+                telemetry.addLine("Running Trajectory:"+ trajectory);
+            }else if(gamepad1.x && Objects.equals(trajectory,"RedLeft")){
+                roadrunnerhandeler.drive.followTrajectorySequence(RoadRunnerHandler.RedLeftSideRunner);
+                telemetry.addLine("Running Trajectory:"+ trajectory);
+            }else if(gamepad1.x && Objects.equals(trajectory, "RedRight")){
+                roadrunnerhandeler.drive.followTrajectorySequence(RoadRunnerHandler.RedRightSideRunner);
+                telemetry.addLine("Running Trajectory:"+ trajectory);
+            }
+
 
 
             // Show the elapsed game time and wheel power.
